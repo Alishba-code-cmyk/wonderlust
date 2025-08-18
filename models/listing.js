066fa8b1ad21152f1e75a15bd363f239310be971
Alishba-code-cmyk <itsalishba00@gmail.com>
2025-08-18 22:16:24 +0530

    Update listing.js

--- models/listing.js
+++ models/listing.js
@@ -6,15 +6,17 @@ const listingSchema=new schema({
         required:true,},
     description: String,
     image:{
-        filename: String,
-        url:{
         type:String,
-        default:"https://unsplash.com/photos/lighthouse-stands-tall-on-a-rocky-coastline-QdDg2X_2gj0",
- set:(v)=>v===""?"https://unsplash.com/photos/lighthouse-stands-tall-on-a-rocky-coastline-QdDg2X_2gj0":v,
+        default: "https://unsplash.com/photos/lighthouse-stands-tall-on-a-rocky-coastline-QdDg2X_2gj0",
+        set:(v) =>
+             v === ""
+         ? "https://unsplash.com/photos/lighthouse-stands-tall-on-a-rocky-coastline-QdDg2X_2gj0"
+         :v,
 },
-    },
+    
     price: Number ,
     country:String,
+    location:String,
 });
-const listing=mongoose.model("listing",listingSchema);
-module.exports=listing;
+const Listing=mongoose.model("Listing",listingSchema);
+module.exports=Listing;
