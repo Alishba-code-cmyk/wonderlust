e3d6ff1cc07100f96403aa21f5969425d5e9156d
alishba <itsalishba00@gmail.com>
2025-09-08 22:40:38 +0530

    add review validation

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ const ejsMate=require("ejs-mate");
 const wrapAsync=require("./utils/wrapAsync.js");// custom wrapAsync
 const ExpressError=require("./utils/ExpressError.js");
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
-const {listingSchema}=require("./schema.js");
+const {listingSchema, reviewSchema}=require("./schema.js");
 const Review=require("./models/review.js");
+
 main().then(()=>{
     console.log("connected to db");
 })
@@ -33,6 +34,15 @@ app.get("/",(req,res)=>{
 
 const validateListing=(req,res,next)=>{
      let {error}= listingSchema.validate(req.body);
+  if (error){
+    let errMsg= error.details.map((el)=>el.message).join(",");
+    throw new ExpressError(400,errMsg);
+  }else{
+    next();
+  }
+};
+const validateReview=(req,res,next)=>{
+     let {error}= reviewSchema.validate(req.body);
   if (error){
     let errMsg= error.details.map((el)=>el.message).join(",");
     throw new ExpressError(400,errMsg);
@@ -86,14 +96,15 @@ app.delete("/listings/:id",wrapAsync( async (req,res)=>{
     res.redirect("/listings");
 }));
 //review route
-app.post("/listings/:id/reviews", async(req,res)=>{
+app.post("/listings/:id/reviews",validateReview,
+    wrapAsync(async(req,res)=>{
    let listing= await Listing.findById(req.params.id);
    let newReview=new Review(req.body.review);
    listing.reviews.push(newReview);
    await newReview.save();
    await listing.save();
    res.redirect(`/listings/${listing._id}`);
-});
+}));
 // app.get("/testlisting",async (req,res)=>{
 // let samplelisting=new Listing({
 //    title:"my home",
