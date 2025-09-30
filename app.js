57676f8a7c32ac0167b9f4fb4dc2f79003010fab
alishba <itsalishba00@gmail.com>
2025-10-01 01:56:32 +0530

    final

--- app.js
+++ app.js
@@ -81,8 +81,8 @@ passport.deserializeUser(User.deserializeUser());
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
-  res.locals.currUser= req.user;
-  next();
+  res.locals.currUser= req.user||null;
+    next();
 });
 
 // app.get("/demouser",async(req,res)=>{
